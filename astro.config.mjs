133aaec623261f2223b9fcf2ca35eb605e40e3c3
Aleks <gajim91@gmail.com>
2025-02-06 13:56:02 -0600

    Intentando installar tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
-import { defineConfig } from "astro/config";
+// @ts-check
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
+// https://astro.build/config
 export default defineConfig({
-  // ...
-
-  integrations: [
-    tailwind({
-      configFile: "./tailwind.config.mjs",
-    }),
-  ],
+  integrations:[tailwind()]
 });
